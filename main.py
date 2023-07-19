b7f5bd5329703fd904efbf7f9f1988c32de38e76
aegerita <jennytai3221@gmail.com>
2023-07-19 10:39:37 -0400

    save space

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 from bs4 import BeautifulSoup
+from bs4 import NavigableString
 
 def fixWord(soup, line, word):
     word = list(word)
 
     bold_part = BeautifulSoup.new_tag(soup, 'b')
-    pre = BeautifulSoup.new_tag(soup, 'span')
-    post = BeautifulSoup.new_tag(soup, 'span')
-    # bold_part['class'] = 'bd'
-    pre['class'] = 'txt'
-    post['class'] = 'txt'
+    pre = NavigableString('')
+    post = NavigableString('')
 
     # append first half to bold part
     counter = 0
     for char in word:
         if counter == 0 and not char.isalpha():
-            pre.append(char)
+            pre += char
         elif counter < len(word)//2:
             counter += 1
             bold_part.append(char)
         else:
-            post.append(char)
-    line.extend([pre, bold_part, post, ' '])
+            post += char
+    if bold_part.get_text() == '': line.extend([pre, post, ' '])
+    else: line.extend([pre, bold_part, post, ' '])
 
 with open('input.html') as html_file:
     soup = BeautifulSoup(html_file.read(), features='html.parser')
@@ -28,13 +27,14 @@ with open('input.html') as html_file:
     # add style
     head = soup.head
     head.append(soup.new_tag('style', type='text/css'))
-    head.style.append('b { line-height: 2; font-family: Helvetica; font-weight: bold; } .txt { font-family: Helvetica;font-weight: bold; color: #666666; } body {background-color:#edd1e0;}')
+    head.style.append('p.m { font-family: Helvetica;font-weight: bold; color: #666666; } p.m b { line-height: 2; font-family: Helvetica; font-weight: bold; color: #111111 } body {background-color:#edd1e0;}')
 
     for tag in soup.findAll('p'):
         # Remove all <i> tags - TODO: fix this
         if tag.string is None and tag.findAll('i') == []: continue
         line = BeautifulSoup.new_tag(soup, 'p')
         line.attrs = tag.attrs
+        line.attrs['class'] = 'm'
         for word in tag.get_text().split(' '):
             fixWord(soup, line, word)
         tag.replace_with(line)
